ed8548a6b89fde534638fc0879dd6dc0f3973c1b
abhaysarkar <abhaysheets@gmail.com>
2024-08-02 16:40:29 +0530

    new changes

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -25,7 +25,7 @@ const MainPage = () => {
   const itemsPerPage = 8;
 
   useEffect(() => {
-    axios.get('http://localhost:5000/show-all')
+    axios.get('http://reclaim-env.eba-gpvhizmd.ap-south-1.elasticbeanstalk.com/show-all')
                
       .then(response => {
         setData(response.data);
@@ -69,7 +69,7 @@ const MainPage = () => {
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:5000/add-new-problem', newProblem)
+    axios.post('http://reclaim-env.eba-gpvhizmd.ap-south-1.elasticbeanstalk.com/add-new-problem', newProblem)
       .then(response => {
         setData([...data, response.data]);
         setFilteredData([...data, response.data]);
@@ -88,7 +88,7 @@ const MainPage = () => {
   };
 
   const handleReviseClick = () => {
-    axios.get('http://localhost:5000/get-random-one')
+    axios.get('http://reclaim-env.eba-gpvhizmd.ap-south-1.elasticbeanstalk.com/get-random-one')
       .then(response => {
         setRandomProblem(response.data);
         setShowReviseModal(true);
